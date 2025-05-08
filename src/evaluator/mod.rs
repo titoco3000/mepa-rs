afff5b292a7fab7769f0948295c0dafd3cb6efa3
tito guidotti <tito.guidotti@gmail.com>
2025-05-08 14:45:44 -0300

    Muda banco de exemplos

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -42,16 +42,56 @@ pub fn evaluate() {
     }
 
     let work_material = [
-        ("acesso-aleatorio", vec![1], vec![20]),
-        ("cod-morto", vec![], vec![]),
-        ("copia", vec![], vec![12]),
-        ("deadvar", vec![13], vec![13]),
-        ("global-var", vec![], vec![7]),
-        ("indirecao", vec![], vec![]),
-        ("inner", vec![], vec![20]),
-        ("lifetime", vec![], vec![]),
-        ("movimentacao", vec![], vec![0, 1, 2, 3, 4]),
-        ("sort", vec![], vec![2, 1, 3, 2, 1, 3, 1, 2, 3]),
+        ("elementares/acesso-aleatorio", vec![1], vec![20]),
+        ("elementares/cod-morto", vec![], vec![]),
+        ("elementares/copia", vec![], vec![12]),
+        ("elementares/deadvar", vec![13], vec![13]),
+        ("elementares/global-var", vec![], vec![7]),
+        ("elementares/indirecao", vec![], vec![]),
+        ("elementares/inner", vec![], vec![20]),
+        ("elementares/lifetime", vec![], vec![]),
+        ("elementares/movimentacao", vec![], vec![0, 1, 2, 3, 4]),
+        ("algoritmos/binary_search", vec![], vec![4]),
+        (
+            "algoritmos/bubble_sort",
+            vec![],
+            vec![2, 1, 3, 2, 1, 3, 1, 2, 3],
+        ),
+        (
+            "algoritmos/decimal_to_binary",
+            vec![123],
+            vec![1, 1, 0, 1, 1, 1, 1],
+        ),
+        (
+            "algoritmos/euler_spigot",
+            vec![],
+            vec![
+                2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4, 5, 9, 0, 4, 5, 2, 3, 5, 3, 6, 0, 2, 8, 7, 4, 7, 1,
+                3, 5, 2, 6, 6, 2, 4, 9, 7, 7, 5, 7, 2, 4, 7, 0, 9, 3, 6, 9, 9, 9, 5, 9, 5, 7, 4, 9,
+                6, 6, 9, 6, 7, 6, 2, 7, 7, 2, 4, 0, 7, 6, 6, 3, 0, 3, 5, 3, 5, 4, 7, 5, 9, 4, 5, 7,
+                1, 3, 8, 2, 1, 7, 8, 5, 2, 5, 1, 6, 6, 4, 2, 7, 4,
+            ],
+        ),
+        ("algoritmos/fibonacci", vec![30], vec![832040]),
+        //("algoritmos/floyd_warshall", vec![], vec![]),
+        (
+            "algoritmos/merge_sort",
+            vec![],
+            vec![38, 27, 43, 10, 3, 3, 10, 27, 38, 43],
+        ),
+        ("algoritmos/quicksort", vec![], vec![1, 5, 7, 8, 9, 10]),
+        (
+            "algoritmos/rng",
+            vec![],
+            vec![306, 437, 4554, 4199, 2142, 3119, 4584, 4517, 654, 2435],
+        ),
+        (
+            "algoritmos/tower_of_hanoi",
+            vec![],
+            vec![
+                1, 1, 2, 2, 1, 3, 1, 2, 3, 3, 1, 2, 1, 3, 1, 2, 3, 2, 1, 1, 2,
+            ],
+        ),
     ];
 
     for (filename, input, expected_output) in work_material.iter() {
